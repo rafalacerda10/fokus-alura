c03ff8d019a44f88d77715625656e6db6a960796
Rafael Fermino <rafael_fermino_10@hotmail.com>
2023-11-21 10:05:06 -0300

    Change elements

--- script.js
+++ script.js
@@ -5,15 +5,30 @@ const btnFoco = document.querySelector('.app__card-button--foco');
 const btnCurto = document.querySelector('.app__card-button--curto');
 const btnLongo = document.querySelector('.app__card-button--longo');
 
+// Variavel imagem para realizar a troca de imagens junto
+const banner = document.querySelector('.app__image');
+
+
 //Interatividade entre as páginas alterando o atributos do html 
 btnFoco.addEventListener('click',() => {
-    html.setAttribute('data-contexto', 'foco')
+    // html.setAttribute('data-contexto', 'foco')
+    // banner.setAttribute('src', '/imagens/foco.png')
+    alterarContexto('foco')
 })
 
 btnCurto.addEventListener('click', () => {
-    html.setAttribute('data-contexto','descanso-curto')
+    // html.setAttribute('data-contexto','descanso-curto')
+    // banner.setAttribute('src','/imagens/descanso-curto.png')
+    alterarContexto('descanso-curto')
 })
 btnLongo.addEventListener('click',() =>{
-    html.setAttribute('data-contexto', 'descanso-longo')
+    // html.setAttribute('data-contexto', 'descanso-longo')
+    // banner.setAttribute('src','/imagens/descanso-longo.png')
+    alterarContexto('descanso-longo')
 })
+
+function alterarContexto(contexto) {
+    html.setAttribute('data-contexto', contexto)
+    banner.setAttribute('src',`/imagens/${contexto}.png`)
+}
 
